6c78cbdc2cf721e71652ce042884ddc111449860
patrickpie <patrickpie830907@gmail.com>
2023-11-26 21:20:11 +0800

    import js修正

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import './assets/js/aos';
-import './assets/js/index';
+import './assets/js/aos.js';
+import './assets/js/index.js';
 
 console.log("Hello world!");
