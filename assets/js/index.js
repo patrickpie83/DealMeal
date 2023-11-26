e6a4fb0c67ee5a3e4d432e3d16180c3ff2168193
patrickpie <patrickpie830907@gmail.com>
2023-11-26 23:14:39 +0800

    backtotop功能修正

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,8 +12,13 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+backToTopBtn.addEventListener("click",function() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+})
+
+// When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0; // For Safari
+//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
