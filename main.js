973d5d890e69ade691ffefd8c8fdbe4253aac5b5
patrickpie <patrickpie830907@gmail.com>
2023-12-02 16:47:06 +0800

    js檔測試

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import './assets/js/aos.js';
 import './assets/js/index.js';
 
-import './assets/login.js'; 
+import './assets/js/login.js'; 
 // 最一開始的狀態，login.html直接引入login.js
