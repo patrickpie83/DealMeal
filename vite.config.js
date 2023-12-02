bd21b266ef4516885bf870926563a343f022d620
patrickpie <patrickpie830907@gmail.com>
2023-12-02 17:29:05 +0800

    js檔測試

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { glob } from 'glob';
 
 import liveReload from 'vite-plugin-live-reload';
 
-import copy from 'rollup-plugin-copy';
+// import copy from 'rollup-plugin-copy';
 
 function moveOutputPlugin() {
   return {
@@ -31,13 +31,13 @@ export default defineConfig({
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
-    moveOutputPlugin(),
+    moveOutputPlugin()
 
     //將 vite config 設定讓資料能進入 dist
-    copy({
-      targets: [{ src: 'assets/**/*.js', dest: 'dist/assets' }],
-      hook: 'writeBundle',
-    }),
+    // copy({
+    //   targets: [{ src: 'assets/**/*.js', dest: 'dist/assets' }],
+    //   hook: 'writeBundle',
+    // }),
   ],
   server: {
     // 啟動 server 時預設開啟的頁面
